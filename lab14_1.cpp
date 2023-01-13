218b2f7ba021eeef4eca13180bba8c03aaee7f54
thanaschai <thanaschai@gmail.com>
2023-01-13 16:30:19 +0700

    DONE

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,30 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[] ,int N ,double B[]){
+    double sum1 , sum2 ,sum3 = 1 ,sum4 ,min = A[0] ,max = A[0];
+
+    for (int i = 0 ; i < N ;i++ ){
+    sum1 = sum1 + A[i] ;
+    sum2 = sum2 + pow(A[i],2) ;
+    sum3 = sum3 * A[i] ;
+    sum4 = sum4 + 1/A[i] ;
+    if (A[i] < min )
+    {
+        min = A[i] ;
+    }
+    if (A[i] > max)
+    {
+        max = A[i] ;
+    }
+
+    }
+    B[0] = sum1 / N ;
+    B[1] = sqrt((sum2 / N)- (pow(B[0],2)));
+    B[2] = pow(sum3,1.0/N) ;
+    B[3] = N / sum4;
+    B[4] = max ;
+    B[5] = min ;
+
+}
