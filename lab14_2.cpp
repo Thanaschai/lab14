a46a7954ae4611322715a834dba34bd3f7a4615d
thanaschai <thanaschai@gmail.com>
2023-01-13 17:10:10 +0700

    DONE

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,35 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void updateImage(bool M[][M] ,int s,int X,int Y){
+    for (int i = 0; i < N ;i++)
+    {
+        for (int j =0 ;j < ::M ;j++)
+        {
+            if ( sqrt((pow(i-X,2))+(pow(j-Y,2))) <= s -1){
+                M[i][j] = 1 ;
+            }
+        }
+    }
+
+}
+void showImage(const bool boo[][M]){
+    cout << "------------------------------------------------------------------------" << endl ;
+    for(int i = 0; i < N; i++)
+    {
+        cout << "|" ;
+        for (int j = 0 ; j < M ; j++)
+        {
+            if (boo[i][j] == 0){
+                cout << " " ;
+
+            }
+            else if(boo[i][j] == 1)
+            {
+                cout << "*";
+            }
+        }
+        cout << "|" << endl;
+    }
+    cout << "------------------------------------------------------------------------" << endl ;
+}
